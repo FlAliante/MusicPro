3bed429f28b876f0f976ab6890b4252f1ff4a698
Felipe Peña <felipe.aliante94@gmail.com>
2023-03-12 21:21:29 -0300

    Se adapto mejor al modelo mvc

--- src/views/cliente.py
+++ src/views/cliente.py
@@ -1,6 +1,9 @@
+from flask import Blueprint, jsonify, make_response, render_template, request
+import json
+from config import db_session
 from sqlalchemy import asc, desc
 from src.models import Product
-from flask import Blueprint, make_response, render_template
+
 
 view_cliente = Blueprint("view_cliente", __name__)
 
@@ -14,26 +17,60 @@ def guitarras():
 def bajos():
     return render_template("cliente/bajos.html")
 
+@view_cliente.route("/productos")
+def productos():
+    return render_template("cliente/productos.html", productos=productos)
 
-@view_cliente.route("/portfolio/list", methods=["POST"])
-def list():
+
+@view_cliente.route("/list_products", methods=["GET"])
+def table_data():
     try:
-        my_list = []
-        products = Product.query.all()
-        total_list = len(products)
+        search = request.args.get("search")
+        sort = request.args.get("sort")
+        sort = "price"
+        limit = request.args.get("limit")
+        offset = request.args.get("offset")
 
-        for item in products:
-            property = {
-                "id": item.id,
-                "title": item.description,
-                # 'url': item.url,
-                "photo": item.photo,
-                "price": item.price,
-            }
-            my_list.append(property)
-        return {"rows": my_list, "total": total_list}
+        # obtengo resultados
+        total_list = Product.query.filter(Product.description.ilike(f'%{search}%')).count() if search else Product.query.count()
+        product_page = Product.query.filter(Product.description.ilike(f'%{search}%')).order_by(Product.price.desc() if sort == 'desc' else Product.price.asc()).offset(int(offset)).limit(int(limit)).all()
+
+        # serializar resultados
+        serialized_list = [json.loads(json.dumps(product.to_json())) for product in product_page]
+
+        # armar el objeto json response
+        response = {"rows": serialized_list, "total": total_list}
+        return jsonify(response)
+
     except Exception as e:
         print(str(e))
         return make_response(str(e), 500)
-    #finally:
-        #return {"rows": my_list, "total": total_list}
+    finally:
+        # siempre cerrar la sesión, independientemente del resultado
+        db_session.close_all()
+
+@view_cliente.route("/portfolio/list", methods=["GET"])
+def get_table_data():
+    try:
+        products = Product.query.all()
+        serialized_list = []
+        for product in products:
+            serialized_product = {
+                "id": product.id,
+                "description": product.description,
+                "create_time": str(product.create_time),
+                "update_time": str(product.update_time),
+                "price": product.price,
+                "photo": product.photo,
+                "url_nintendo": product.url_nintendo,
+            }
+
+            serialized_list.append(serialized_product)
+        return jsonify(serialized_list)
+
+    except Exception as e:
+        print(str(e))
+        return make_response(str(e), 500)
+    finally:
+        # siempre cerrar la sesión, independientemente del resultado
+        db_session.close_all()
