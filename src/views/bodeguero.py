bf357b6a9aefcacaa754aab436792e959cc36cd5
Felipe Peña <felipe.aliante94@gmail.com>
2023-03-15 03:20:29 -0300

    Segmentacion de tablas, ya que, no esta pidiendo filtros en los productos

--- src/views/bodeguero.py
+++ src/views/bodeguero.py
@@ -2,39 +2,9 @@ from sqlalchemy import asc, desc
 from src.models import Product
 from flask import Blueprint, render_template
 
-bodeguero_demo = Blueprint("bodeguero_demo", __name__)
+view_bodeguero = Blueprint("bodeguero_demo", __name__)
 
 
-@bodeguero_demo.route("/checkout.html")
+@view_bodeguero.route("/inventario.html")
 def checkout():
-    return render_template("demo/checkout.html")
-
-
-@bodeguero_demo.route("/codes.html")
-def codes():
-    return render_template("demo/codes.html")
-
-
-@bodeguero_demo.route("/contact.html")
-def contact():
-    return render_template("demo/contact.html")
-
-
-@bodeguero_demo.route("/electronics.html")
-def electronics():
-    return render_template("demo/electronics.html")
-
-
-@bodeguero_demo.route("/mens.html")
-def mens():
-    return render_template("demo/mens.html")
-
-
-@bodeguero_demo.route("/single.html")
-def single():
-    return render_template("demo/single.html")
-
-
-@bodeguero_demo.route("/womens.html")
-def womens():
-    return render_template("demo/womens.html")
+    return render_template("bodeguero/inventario.html")
